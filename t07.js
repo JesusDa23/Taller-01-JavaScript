5a30d08bc2487077aa4136f5132a56f7072467d5
Jesús David Chavarro Castro <jesusdavidchavarro23@gmail.com>
2024-04-29 20:52:34 -0500

    arreglos en un ejercicio

--- t07.js
+++ t07.js
@@ -1,9 +1,17 @@
 // 7. Escribe un programa que tome un string como entrada y cuente el número de vocales que contiene
 
 let palabra = prompt("ingresa una palabra con vocales: ");
-const vocales = ["a", "e", "i", "o", "u"];
+const vocales = "aeiou";
+let contar = 0;
+
+for(i = 0; i < palabra.length; i++)
+if (palabra[i] === vocales[i]){
+    contar++
+}
+
+console.log(contar)
 
 
-let resultado = palabra.includes(vocales.length);
+// let resultado = palabra.includes(vocales.length);
 
-console.log(resultado)
+// console.log(resultado)
